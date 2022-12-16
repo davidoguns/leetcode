7a97b066011f07544c852ec2f0a6184986de96cc
David Oguns <davidoguns@gmail.com>
2022-12-16 01:04:49 -0800

    Added tests.

--- rust/romanToInt/src/lib.rs
+++ rust/romanToInt/src/lib.rs
@@ -6,6 +6,11 @@ pub enum ConversionError {
     InvalidRomanNumber,
 }
 
+/** 
+ * Incompleteness here is that there is no actual checking that the Roman numeral
+ * digits are being used in proper order. It accepts input like "IIMM" and thinks
+ * it's 2000, when it's really invalid.
+ */
 pub fn roman_to_int(input: &str) -> Result<i32, ConversionError> {
     let mut cumulation: i32 = 0;
     let mut skip = false;
@@ -17,7 +22,7 @@ pub fn roman_to_int(input: &str) -> Result<i32, ConversionError> {
         if let Some(value) = roman_digit_to_int(&c) {
             to_add = value;
             if let Some(next_char) = character_vec.get(index+1) {
-                if let Some(next_value) = roman_digit_to_int(next_char) {
+                if let Some(next_value) = roman_digit_to_int(&next_char) {
                     if next_value > value { //unspecific generalization of prefixes
                         to_add = next_value - value;
                         skip = true;
@@ -38,13 +43,13 @@ pub fn roman_to_int(input: &str) -> Result<i32, ConversionError> {
 
 pub fn roman_digit_to_int(digit: &char) -> Option<i32> {
     match digit {
-        'I' => Some(1),
-        'V' => Some(5),
-        'X' => Some(10),
-        'L' => Some(50),
-        'C' => Some(100),
-        'D' => Some(500),
-        'M' => Some(1000),
+        'I' | 'i' => Some(1),
+        'V' | 'v' => Some(5),
+        'X' | 'x' => Some(10),
+        'L' | 'l' => Some(50),
+        'C' | 'c' => Some(100),
+        'D' | 'd' => Some(500),
+        'M' | 'm' => Some(1000),
         _ => None
     }
 }
--- rust/romanToInt/tests/mod.rs
+++ rust/romanToInt/tests/mod.rs
@@ -1,17 +1,48 @@
 #[cfg(test)]
 mod tests {
     use roman_to_int::roman_to_int;
+    use roman_to_int::roman_digit_to_int;
 
     #[test]
-    fn not_a_roman() {
+    fn invalid_roman_numbers() {
+        assert_eq!(true, roman_to_int("2#$^%&&").is_err());
         assert_eq!(true, roman_to_int("not_a_roman").is_err());
         assert_eq!(true, roman_to_int("-").is_err());
     }
 
     #[test]
-    fn valid_values() {
+    fn invalid_digits() {
+        assert_eq!(None, roman_digit_to_int(&'\n'));
+        assert_eq!(None, roman_digit_to_int(&'p'));
+        assert_eq!(None, roman_digit_to_int(&'-'));
+    }
+
+    #[test]
+    fn valid_digits() {
+        assert_eq!(Some(1), roman_digit_to_int(&'i'));
+        assert_eq!(Some(1), roman_digit_to_int(&'I'));
+        assert_eq!(Some(5), roman_digit_to_int(&'v'));
+        assert_eq!(Some(5), roman_digit_to_int(&'V'));
+        assert_eq!(Some(10), roman_digit_to_int(&'x'));
+        assert_eq!(Some(10), roman_digit_to_int(&'X'));
+        assert_eq!(Some(50), roman_digit_to_int(&'l'));
+        assert_eq!(Some(50), roman_digit_to_int(&'L'));
+        assert_eq!(Some(100), roman_digit_to_int(&'c'));
+        assert_eq!(Some(100), roman_digit_to_int(&'C'));
+        assert_eq!(Some(500), roman_digit_to_int(&'d'));
+        assert_eq!(Some(500), roman_digit_to_int(&'D'));
+        assert_eq!(Some(1000), roman_digit_to_int(&'m'));
+        assert_eq!(Some(1000), roman_digit_to_int(&'M'));
+    }
+
+    #[test]
+    fn valid_entire_numbers() {
         assert_eq!(Ok(0), roman_to_int(""));
         assert_eq!(Ok(1), roman_to_int("I"));
+        assert_eq!(Ok(1), roman_to_int("i"));
         assert_eq!(Ok(4), roman_to_int("IV"));
+        assert_eq!(Ok(14), roman_to_int("xiv"));
+        assert_eq!(Ok(2002), roman_to_int("MMII"));
+        assert_eq!(Ok(2112), roman_to_int("MMCXII"));
     }
 }
