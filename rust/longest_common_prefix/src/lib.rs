791c16ac271a238219007c88f8b95f511ce0381d
David Oguns <davidoguns@gmail.com>
2022-12-25 15:39:42 -0800

    Longest common prefix converted into Rust.

---
+++ rust/longest_common_prefix/src/lib.rs
@@ -0,0 +1,45 @@
+pub fn longest_common_prefix(strings: &[&str]) -> String {
+    if strings.len() == 0 { return "".to_string(); }
+    let mut match_vec = Vec::<(usize, char)>::new();
+    'string_itr: for (string_index, s) in strings.iter().enumerate() {
+        match string_index {
+            0 => {
+                //first string the match is entire string
+                s.chars().enumerate().for_each(|(idx, ch)| {
+                    match_vec.push((idx, ch));
+                });
+                println!("match vec for first: {}", s);
+            }
+            _ => {
+                let mut schars_itr = s.chars().into_iter();
+                for ch_tup in &match_vec {
+                    if let Some(schar) = schars_itr.next() {
+                        if schar != (*ch_tup).1 {
+                            _ = match_vec.split_off((*ch_tup).0);
+                            continue 'string_itr;
+                        }
+                    }
+                    else {
+                        continue 'string_itr;
+                    }
+                }
+            }
+        }
+
+        if match_vec.is_empty() {
+            break;
+        }
+    }
+
+    match_vec.into_iter().map(|(_, ch)| { ch }).collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_prefixes() {
+        assert_eq!("ab", longest_common_prefix(&["abcd", "ab", "abdcdef"]));
+    }
+}
