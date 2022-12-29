2145979291eaf1c889da89e1bc83e4336d71214c
David Oguns <davidoguns@gmail.com>
2022-12-28 19:59:44 -0800

    Restructure directory naming.

---
+++ rust/longest_common_prefix/src/bin/prefix_finder.rs
@@ -0,0 +1,29 @@
+use std::io::{self, Write};
+use longest_common_prefix::longest_common_prefix;
+
+fn main() -> ! {
+    let mut buffer = String::new();
+    loop {
+        println!("Enter a comma delimited string to find the longest common prefix for: ");
+        io::stdout().flush().unwrap();
+        match io::stdin().read_line(&mut buffer) {
+            Ok(_) => {
+                let trimmed = buffer.trim();
+                if String::from("q").eq_ignore_ascii_case(&trimmed) ||
+                    String::from("quit").eq_ignore_ascii_case(&trimmed) {
+                    std::process::exit(0);
+                }
+                else {
+                    let input: Vec<&str> = trimmed.split(',').collect();
+                    let lcp = longest_common_prefix(&input);
+
+                    println!("Longest common prefix is: {}", lcp);
+                }
+            },
+            Err(e) => {
+                println!("Error reading line: {}", e);
+            }
+        }
+        buffer.clear();
+    }
+}
--- rust/longest_common_prefix/src/lib.rs
+++ rust/longest_common_prefix/src/lib.rs
@@ -1,4 +1,3 @@
-
 pub fn longest_common_prefix(strs: &[&str]) -> String {
     if let Some(first_string) = strs.get(0) {
         let mut split_off_idx = first_string.chars().count();
