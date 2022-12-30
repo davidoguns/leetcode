2e09a65629014f05bfa4e1ae704325832cd6d9ae
David Oguns <davidoguns@gmail.com>
2022-12-29 23:09:22 -0800

    Ready to solve task ordering

--- rust/single_thread_tasker/src/lib.rs
+++ rust/single_thread_tasker/src/lib.rs
@@ -1,5 +1,9 @@
 
-pub fn get_order(tasks: Vec<Vec<i32>>) -> i32 {
+pub fn get_order_vec(tasks: Vec<Vec<i32>>) -> Vec<i32> {
+    get_order(tasks.into_iter().map(|v| { (*v.get(0).unwrap(), *v.get(1).unwrap()) }).collect::<Vec<(i32, i32)>>())
+}
+
+pub fn get_order(_tasks: Vec<(i32, i32)>) -> Vec<i32> {
     unimplemented!()
 }
 
