7e9cb721278945428e0111f77fe246bb754db9d5
David Oguns <davidoguns@gmail.com>
2023-12-19 06:09:01 -0800

    Finished extension of day 3.

--- rust/adventofcode/day3/src/main.rs
+++ rust/adventofcode/day3/src/main.rs
@@ -70,10 +70,12 @@ impl PartsData {
         adjacent_vec 
     }
 
-    //ends up unused?
+    //misleading method really, we only use it to validate that a Location is within
+    //bounds on the board rather than actually doing the math to retrieve a character
+    //at a given flat index
     pub fn get_index(&self, location: &Location) -> Option<usize> {
-        if location.1 > 0 && location.1 < self.rows.try_into().unwrap() &&
-            location.0 > 0 && location.0 < self.columns.try_into().unwrap() {
+        if location.1 >= 0 && location.1 < self.rows.try_into().unwrap() &&
+            location.0 >= 0 && location.0 < self.columns.try_into().unwrap() {
             return Some(location.0 as usize + (location.1 as usize * self.columns))
         }
         None
@@ -85,12 +87,10 @@ impl PartsData {
         //we need a copy at the first character, because loc will still be used to track
         //as we move character by character reading the entire number
         let part_num_loc = loc.clone();
-        // println!("Part num starts at {}, {}", part_num_loc.0, part_num_loc.1);
         let mut value: u32 = first_ch.to_digit(10).expect("Should be a digit");
         let mut length = 1usize;
         while let Some(ch) = ch_itr.next() {
             if ch.is_digit(10) {
-                // println!("Value is {value}");
                 value = (value * 10) + ch.to_digit(10).expect("Should be a digit");
                 length += 1;
                 loc.0 += 1;
@@ -100,8 +100,6 @@ impl PartsData {
                 break;
             }
         }
-        // println!("Part num starts at {}, {}", part_num_loc.0, part_num_loc.1);
-        // println!("Part value and length {}, {}", value, length);
         (value, part_num_loc, length)
     }
 }
@@ -155,11 +153,53 @@ pub fn load_parts_data_from_file(filename: &str) -> Option<(PartsData, Vec<PartN
     }
 }
 
-pub fn identify_parts_numbers(data: (PartsData, Vec<PartNumberLocation>)) -> Vec<u32> {
+pub fn identify_gears(data: &(PartsData, Vec<PartNumberLocation>)) -> Vec<u32> {
+    let mut gear_twos = Vec::<u32>::new();
+    'gear_loop:
+    for (ch_index, ch) in data.0.data.iter().enumerate() {
+        if *ch == '*' {
+            let char_loc: Location = (ch_index as i32 % data.0.columns as i32, ch_index as i32 / data.0.rows as i32);
+            //here we are faking the structure used for numbers so we can re-use the function to
+            //get adjacent cells for numbers. We are setting the value of the number to zero, and
+            //the length of it to one
+            let char_loc_pnl = (0u32, char_loc, 1usize);
+    
+            let adjacents = data.0.get_adjacent_cells(&char_loc_pnl);
+            let mut num_adjacent = 0;
+            let mut gear_power = 1;
+            //now loop through all part numbers and see which ones match
+            'part_num_loop:
+            for pnl in &data.1 {
+                //simple to write early exit test if it's more than one row above or below
+                /* if pnl.1.1 < (char_loc.1-1) || pnl.1.1 > (char_loc.1+1) {
+                    continue;
+                } */
+                //now explictly test collision with each adjacent
+                for adj in &adjacents {
+                    if adj.1 == pnl.1.1 && adj.0 >= pnl.1.0 && adj.0 < (pnl.1.0 + pnl.2 as i32) {
+                        gear_power *= pnl.0;
+                        num_adjacent += 1;
+                        if num_adjacent > 2 {
+                            continue 'gear_loop;
+                        }
+                        continue 'part_num_loop;
+                    }
+                }
+            }
+            //must be exactly two numbers, no less, no more
+            if num_adjacent == 2 {
+                // println!("Found a gear two at: ({}, {}), with magnitude {gear_power}", char_loc.0, char_loc.1);
+                gear_twos.push(gear_power);
+            }
+        }
+    }
+    gear_twos
+}
+
+pub fn identify_parts_numbers(data: &(PartsData, Vec<PartNumberLocation>)) -> Vec<u32> {
     let mut valid_part_numbers = Vec::<u32>::new();
     'part_num_loop:
-    for pnl in data.1 {
-            println!("Getting adjacent cells for: {}", pnl.0);
+    for pnl in &data.1 {
         for adjacent_location in data.0.get_adjacent_cells(&pnl) {
             if let Some(ch) = data.0.data.get(
                         adjacent_location.0 as usize + adjacent_location.1 as usize * data.0.columns) {
@@ -167,9 +207,7 @@ pub fn identify_parts_numbers(data: (PartsData, Vec<PartNumberLocation>)) -> Vec
                     println!("Valid part number: {}; located at ({}, {})", pnl.0, pnl.1.0, pnl.1.1);
                     valid_part_numbers.push(pnl.0);
                     continue 'part_num_loop;
-                } else {
-                    // println!("Not a valid part number: {}", pnl.0);
-                }
+                } else { }
             } else {
                 panic!("Access out of bounds?")
             }
@@ -180,13 +218,18 @@ pub fn identify_parts_numbers(data: (PartsData, Vec<PartNumberLocation>)) -> Vec
 
 fn main() {
     if let Some(data) = load_parts_data_from_file("parts.in") {
-        let parts_numbers = identify_parts_numbers(data);
+        let parts_numbers = identify_parts_numbers(&data);
         let mut sum = 0u32;
         for part_number in parts_numbers {
             sum += part_number;
         }
-
         println!("Sum of all parts numbers is: {sum}");
+
+        let mut sum_of_gear_twos = 0u32;
+        for gear_two in identify_gears(&data) {
+            sum_of_gear_twos += gear_two;
+        }
+        println!("Sum of all gear twos is: {sum_of_gear_twos}");
     } else {
         eprintln!("Could not load parts data from file!");
     }
