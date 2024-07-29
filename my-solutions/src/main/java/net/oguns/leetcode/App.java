3905ba589ca71b6ed2e24a9f4676de46ac883ed2
David Oguns <davidoguns@gmail.com>
2024-07-29 12:37:28 -0700

    add: java structure for solutions.

---
+++ my-solutions/src/main/java/net/oguns/leetcode/App.java
@@ -0,0 +1,13 @@
+package net.oguns.leetcode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
