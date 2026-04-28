b2210f8feb84d4b091265a40718247ef55a2bab2
David Oguns <davidoguns@gmail.com>
2026-04-27 23:57:10 -0700

    upd: Optimized a bit of the solution. Seems to currently beat 100% of leetcode solutions.

--- rust/merge_sorted/src/main.rs
+++ rust/merge_sorted/src/main.rs
@@ -6,47 +6,48 @@ impl Solution {
     pub fn merge(nums1: &mut Vec<i32>, m: i32, nums2: &mut Vec<i32>, n: i32) {
         let m: usize = usize::try_from(m).expect(format!("Cannot convert m[={m}] into usize").as_str());
         let n: usize = usize::try_from(n).expect(format!("Cannot convert n[={n}] into usize").as_str());
-        let mut base_dest_idx: usize = 0;
+        let mut dest_idx: usize = 0;
         let mut nums2_idx: usize = 0;
-        let mut nums1_idx_shift: usize = 0;
+        let mut nums1_drained: usize = 0;
         //insert val, at index into vector. Shift all elements at idx
         //and beyond down 1 index. Loses a value at the end of the vector.
         //TODO: Rust probably has an ergonomic vec function for this.
+        //TODO: We also can limit the number of "shift right" that occurs
+        //      to only operate on the remaining left from nums1 vector
+        //      instead of going to the very end of the vector always.
         let fn_shift_insert = |val: i32, idx: usize, vec: &mut Vec<i32>| {
             //we will shift first by reverse iterating
-            let shift_rng = (idx..vec.len()).rev();
-            println!("Shifting insert range: {:?}", shift_rng);
-            for shift_idx in shift_rng.rev().skip(1) {
-                println!("Shift idx: {shift_idx}");
-                vec[shift_idx] = vec[shift_idx-1];
+            let shift_rng = (idx..vec.len()).skip(1).rev();
+            for shift_idx in shift_rng {
+                if shift_idx as i32 - 1 >= 0 {
+                    vec[shift_idx] = vec[shift_idx-1];
+                }
             }
             vec[idx] = val;
         };
         loop {
-            if nums1_idx_shift >= m {
-                println!("Detected 1st array drained, finished work");
+            if nums1_drained >= m {
                 //means everything left in nums2 goes into nums1
                 //so we fill from where dest_idx is now to the end
                 for idx in nums2_idx..n {
-                    nums1[base_dest_idx + nums1_idx_shift] = nums2[idx];
-                    base_dest_idx += 1;
+                    nums1[dest_idx] = nums2[idx];
+                    dest_idx += 1;
                 }
                 break; //this is a finishing operation
             }
             if nums2_idx >= n {
-                println!("Detected 2nd array drained, finished work");
                 //means we've drained all of the 2nd array into the first
                 //and there's no more work to do
                 break;
             }
             //core logic, compare what's next in either nums1 and nums2
-            if nums2[nums2_idx] < nums1[base_dest_idx + nums1_idx_shift] {
-                fn_shift_insert(nums2[nums2_idx], base_dest_idx + nums1_idx_shift, nums1);
+            if nums2[nums2_idx] < nums1[dest_idx] {
+                fn_shift_insert(nums2[nums2_idx], dest_idx, nums1);
                 nums2_idx += 1;
             } else {
-                nums1_idx_shift += 1;
+                nums1_drained += 1;
             }
-            base_dest_idx += 1;
+            dest_idx += 1;
         }
     }
 }
